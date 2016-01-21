5d03ff5ca546d5431b2c2f0a9b04c2852fa065a2
Anthony Nguyen <anknguyen@gmail.com>
2016-01-21 14:00:04 -0500

    Add item removal on click

--- app/src/main/java/ca/anthonyn/stodo/TodoAdapter.java
+++ app/src/main/java/ca/anthonyn/stodo/TodoAdapter.java
@@ -1,5 +1,7 @@
 package ca.anthonyn.stodo;
 
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,26 +11,44 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder> {
-    ArrayList<TodoItem> items;
+    final ArrayList<TodoItem> items;
+    private static Snackbar snackbar;
 
     public TodoAdapter(ArrayList<TodoItem> items) {
         this.items = items;
     }
 
     class TodoViewHolder extends RecyclerView.ViewHolder {
+        public CardView card;
         public TextView mainTextView;
 
         public TodoViewHolder(View itemView) {
             super(itemView);
-            this.mainTextView = (TextView) itemView.findViewById(R.id.card_main_text);
+            card = (CardView) itemView;
+            mainTextView = (TextView) itemView.findViewById(R.id.card_main_text);
         }
     }
 
     @Override
     public TodoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_layout, parent, false);
-        //view.setOnClickListener();
-        TodoViewHolder viewHolder = new TodoViewHolder(view);
+        final TodoViewHolder viewHolder = new TodoViewHolder(view);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = viewHolder.getAdapterPosition();
+                if (snackbar != null)
+                    snackbar.dismiss();
+
+                snackbar = Snackbar.make(v, "Item removed: " + items.get(position).data, Snackbar.LENGTH_SHORT);
+                snackbar.show();
+
+                items.remove(position);
+                notifyItemRemoved(position);
+            }
+        });
+
         return viewHolder;
     }
 
